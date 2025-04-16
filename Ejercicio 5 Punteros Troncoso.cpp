42272ea667e1248be199f10616d5e8e593ddbc25
TroncosoFernandoG <fernandogtroncoso@gmail.com>
2025-04-16 08:37:29 -0300

    Update Ejercicio 5 Punteros Troncoso.cpp

--- Ejercicio 5 Punteros Troncoso.cpp
+++ Ejercicio 5 Punteros Troncoso.cpp
@@ -1,7 +1,7 @@
 /*
 5. Concatenar dos cadenas usando punteros y funciones.
 
-Troncoso Fernando G 5°1°
+Troncoso Fernando G 5Â°1Â°
 */
 
 #include <stdio.h>
@@ -15,15 +15,15 @@ int main(){
 char cadena[50];
 char cadena2[50];
 	
-	printf("ingresar la 1era cadena de caracteres: ");
+	printf("ingrese la 1era cadena de caracteres: ");
 	fgets(cadena, sizeof(cadena), stdin);
 	cadena[strcspn(cadena, "\n")] = '\0';
 	
-	printf("ingresar la 2da cadena de caracteres: ");
+	printf("ingrese la 2da cadena de caracteres: ");
 	fgets(cadena2, sizeof(cadena2), stdin);
 	cadena2[strcspn(cadena2, "\n")] = '\0';
 	
-	concadena(cadena, cadena2);
+	concatenar(cadena, cadena2);
 	
 	return 0;
 }
@@ -35,4 +35,3 @@ void concatenar(char* cadena, char* cadena2){
 	printf("la cadena concatenada queda asi: \'%s\' ", cadena);
 	
 }
-
