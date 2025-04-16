a1c78a2ace1bc41d073f9053c94800af5b28a997
TroncosoFernandoG <fernandogtroncoso@gmail.com>
2025-04-16 08:36:49 -0300

    Update Ejercicio 4 Punteros Troncoso.cpp

--- Ejercicio 4 Punteros Troncoso.cpp
+++ Ejercicio 4 Punteros Troncoso.cpp
@@ -2,40 +2,37 @@
 4. Copiar un arreglo en otro arreglo usando punteros y funciones. Mostrando
 el resultado final.
 
-Troncoso Fernando G 5°1°
+Troncoso Fernando G 5Â°1Â°
 */
 
 #include <stdio.h>
+#include <string.h>
 
 int i;
 int n;
 
-void copi(int *arr1, int *arr2);
+void copiar(char *cadena, char *cadena2);
 
 int main(){
-    printf("Ingrese la cantidad de numeros que tienen los arreglos: \n");
-    scanf("%d", &n);
-
-    int arr1[n];
-    int arr2[n];
-
-    for(i=0;i<n;i++){
-        printf("ingrese el numero en el espacio %d\n", i + 1);
-        scanf("%d", &arr1[i]);
-    }
-
-    copi(arr1, arr2);
-
-    return 0;
+	
+	char cadenaOri[50];
+	char cadenaCopia[50];
+	
+	printf("ingresar la 1era cadena de caracteres: \n ");
+	fgets(cadenaOri, sizeof(cadenaOri), stdin);
+	cadenaOri[strcspn(cadenaOri, "\n")] = '\0';
+	
+	
+	copiar(cadenaOri, cadenaCopia);
+	
+	
+	return 0;
 }
 
-void copi(int *arr1, int *arr2){
-    for(i=0;i<n;i++){
-        *(arr2 + i) = *(arr1 + i);
-    }
-
-    printf("El arreglo copiado es:\n");
-    for(i=0;i<n;i++){
-        printf("[%d]\t", *(arr2 + i));
-    }
+void copiar(char *cadena, char *cadena2){
+	
+	strcpy(cadena2, cadena);
+	
+	printf("la cadena conpia queda asi: \' %s \' ", cadena2);
+	
 }
